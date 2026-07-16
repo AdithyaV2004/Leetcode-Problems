b3c885d611dc07538af0ad843ff5e572fdd745b8
Adithya_V <83355610+AdithyaV2004@users.noreply.github.com>
2026-07-16 11:54:16 +0530

    Time: 405 ms (25.15%), Space: 33.3 MB (97.6%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.py
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.py
@@ -3,21 +3,16 @@ class Solution:
         return a if b==0 else gcd(b, a%b)
     def gcdSum(self, nums: list[int]) -> int:
         sum=0
-        prefix=[]
-        prefixgcd=[]
         l=len(nums)
         g=nums[0]
-        prefix.append(g)
-        for i in range(1, l):
-            g=max(g, nums[i])
-            prefix.append(g)
         for i in range(l):
-            prefixgcd.append(self.gcd(nums[i], prefix[i]))
-        prefixgcd=sorted(prefixgcd)
+            g=max(g, nums[i])
+            nums[i]=self.gcd(nums[i], g)
+        nums=sorted(nums)
         lp=0
         rp=l-1
         while lp<rp:
-            sum+=self.gcd(prefixgcd[lp], prefixgcd[rp])
+            sum+=self.gcd(nums[lp], nums[rp])
             print(sum)
             lp+=1
             rp-=1
