7682ab9a98a5df15640ef0fb69feb6f0ca5886af
Adithya_V <83355610+AdithyaV2004@users.noreply.github.com>
2026-07-31 09:42:23 +0530

    Time: 95 ms (81.94%), Space: 20.1 MB (23.08%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,24 +1,10 @@
+from collections import Counter
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        d={}
-        for i in word:
-            if i not in d:
-                d[i]=0
-            d[i]+=1
-        f=sorted(d.values())[::-1]
-        l=len(f)
-        rl=[]
-        c=0
-        print(f)
-        for i in range(l):
-            c+=f[i]
-            if i%8==7:
-                rl.append(c)
-                c=0
-        rl.append(c)
-        print(rl)
-        c=0
-        for i in range(len(rl)):
-            c+=(i+1)*rl[i]
-        return c
+        fm=Counter(word)
+        res=0
+        s=sorted(fm.values(), reverse=True)
 
+        for i, v in enumerate(s):
+            res+=v*(i//8+1)
+        return res
