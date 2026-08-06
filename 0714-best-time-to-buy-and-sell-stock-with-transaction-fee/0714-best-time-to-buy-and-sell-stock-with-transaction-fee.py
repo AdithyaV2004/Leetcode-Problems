bd00a8fbe8bf5bf9cf684b0a8e7f60aa59fa9515
Adithya_V <83355610+AdithyaV2004@users.noreply.github.com>
2026-08-06 15:30:55 +0530

    Time: 497 ms (18.77%), Space: 79.1 MB (18.83%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n=len(prices)
+        BUY=0
+        SELL=1
+        dp=[[-1]*2 for _ in range(n+1)]
+        def gamble(idx, transType):
+            if idx==n:
+                return 0
+            if dp[idx][transType]!=-1:
+                return dp[idx][transType]
+            ans1=gamble(idx+1, transType)
+            if transType==BUY:
+                ans2=-(prices[idx]+fee)+gamble(idx+1, SELL)
+            else:
+                ans2=prices[idx]+gamble(idx+1, BUY)
+            dp[idx][transType]=max(ans1, ans2)
+            return dp[idx][transType]
+        return gamble(0, 0)
